01d612491104d9cd79bd42edc257c17b0e482c0f
Pablo Vignoni <pablovignoni@gmail.com>
2025-01-27 19:30:18 -0300

    Implementar una función para actualizar la lista de amigos en la pantalla

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function agregarAmigo(){
     //Obtiene el valor del elemento HTML
     let amigo = elementoHTML.value;
 
-    //Verifica si el valor del elemento HTML no esté vacío
+    //Verifica si el valor del elemento HTML está vacío
     if(amigo == ""){
         //Muestra una alerta
         alert("Por favor, inserte un nombre.");
@@ -21,4 +21,25 @@ function agregarAmigo(){
 
     //Establece el valor del elemento HTML en vacío
     elementoHTML.value = "";
+
+    //Invoca a la función actualizarListaAmigos
+    actualizarListaAmigos();
+}
+
+function actualizarListaAmigos(){
+    //Obtiene el elemento HTML por medio de su id
+    let elementoHTML = document.getElementById("listaAmigos");
+    //Borra todo el contenido dentro del elemento HTML, en este caso, la lista de amigos. Incluso los
+    //elementos hijos de la lista
+    elementoHTML.innerHTML = "";
+
+    //Recorre el arreglo de amigos
+    for(let i = 0; i < amigos.length; i++){
+        //Crea un elemento HTML
+        let elemento = document.createElement("li");
+        //Establece el valor del elemento HTML
+        elemento.innerHTML = amigos[i];
+        //Agrega un elemento hijo al elemento HTML padre
+        elementoHTML.appendChild(elemento);
+    }
 }
